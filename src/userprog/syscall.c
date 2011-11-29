b1b4cb516f1fe888204f8b1ac94986ba77eb14c4
René Kijewski <kijewski@mi.fu-berlin.de>
2011-11-29 07:01:15 +0100

    Many stubs for syscalls

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -12,9 +12,169 @@ syscall_init (void)
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
+#define _SYSCALL_HANDLER_ARGS void              *arg1 UNUSED, \
+                              void              *arg2 UNUSED, \
+                              void              *arg3 UNUSED, \
+                              struct intr_frame *if_  UNUSED
+
 static void
-syscall_handler (struct intr_frame *f UNUSED) 
+syscall_handler_SYS_HALT (_SYSCALL_HANDLER_ARGS)
 {
-  printf ("system call!\n");
-  thread_exit ();
+  //TODO
+}
+
+static void
+syscall_handler_SYS_EXIT (_SYSCALL_HANDLER_ARGS)
+{
+  //TODO
+}
+
+static void
+syscall_handler_SYS_EXEC (_SYSCALL_HANDLER_ARGS)
+{
+  //TODO
+}
+
+static void
+syscall_handler_SYS_WAIT (_SYSCALL_HANDLER_ARGS)
+{
+  //TODO
+}
+
+static void
+syscall_handler_SYS_CREATE (_SYSCALL_HANDLER_ARGS)
+{
+  //TODO
+}
+
+static void
+syscall_handler_SYS_REMOVE (_SYSCALL_HANDLER_ARGS)
+{
+  //TODO
+}
+
+static void
+syscall_handler_SYS_OPEN (_SYSCALL_HANDLER_ARGS)
+{
+  //TODO
+}
+
+static void
+syscall_handler_SYS_FILESIZE (_SYSCALL_HANDLER_ARGS)
+{
+  //TODO
+}
+
+static void
+syscall_handler_SYS_READ (_SYSCALL_HANDLER_ARGS)
+{
+  //TODO
+}
+
+static void
+syscall_handler_SYS_WRITE (_SYSCALL_HANDLER_ARGS)
+{
+  //TODO
+}
+
+static void
+syscall_handler_SYS_SEEK (_SYSCALL_HANDLER_ARGS)
+{
+  //TODO
+}
+
+static void
+syscall_handler_SYS_TELL (_SYSCALL_HANDLER_ARGS)
+{
+  //TODO
+}
+
+static void
+syscall_handler_SYS_CLOSE (_SYSCALL_HANDLER_ARGS)
+{
+  //TODO
+}
+
+static void
+syscall_handler_SYS_MMAP (_SYSCALL_HANDLER_ARGS)
+{
+  //TODO
+}
+
+static void
+syscall_handler_SYS_MUNMAP (_SYSCALL_HANDLER_ARGS)
+{
+  //TODO
+}
+
+static void
+syscall_handler_SYS_CHDIR (_SYSCALL_HANDLER_ARGS)
+{
+  //TODO
+}
+
+static void
+syscall_handler_SYS_MKDIR (_SYSCALL_HANDLER_ARGS)
+{
+  //TODO
+}
+
+static void
+syscall_handler_SYS_READDIR (_SYSCALL_HANDLER_ARGS)
+{
+  //TODO
+}
+
+static void
+syscall_handler_SYS_ISDIR (_SYSCALL_HANDLER_ARGS)
+{
+  //TODO
+}
+
+static void
+syscall_handler_SYS_INUMBER (_SYSCALL_HANDLER_ARGS)
+{
+  //TODO
+}
+
+static void
+syscall_handler (struct intr_frame *if_) 
+{
+  // TODO: ensure readability
+  void *callback = &(*(void ***) &if_->esp)[0];
+  void *nr       = &(*(void ***) &if_->esp)[1];
+  void *arg1     = &(*(void ***) &if_->esp)[2];
+  void *arg2     = &(*(void ***) &if_->esp)[3];
+  void *arg3     = &(*(void ***) &if_->esp)[4];
+  (void) callback;
+  
+  #define _HANDLE(NAME) case NAME: \
+                          syscall_handler_##NAME (arg1, arg2, arg3, if_); \
+                          break;
+  
+  switch (*(int *) nr) {
+    _HANDLE (SYS_HALT);
+    _HANDLE (SYS_EXIT);
+    _HANDLE (SYS_EXEC);
+    _HANDLE (SYS_WAIT);
+    _HANDLE (SYS_CREATE);
+    _HANDLE (SYS_REMOVE);
+    _HANDLE (SYS_OPEN);
+    _HANDLE (SYS_FILESIZE);
+    _HANDLE (SYS_READ);
+    _HANDLE (SYS_WRITE);
+    _HANDLE (SYS_SEEK);
+    _HANDLE (SYS_TELL);
+    _HANDLE (SYS_CLOSE);
+    _HANDLE (SYS_MMAP);
+    _HANDLE (SYS_MUNMAP);
+    _HANDLE (SYS_CHDIR);
+    _HANDLE (SYS_MKDIR);
+    _HANDLE (SYS_READDIR);
+    _HANDLE (SYS_ISDIR);
+    _HANDLE (SYS_INUMBER);
+    default:
+      printf ("Invalid system call!\n");
+      thread_exit ();
+  }
 }
