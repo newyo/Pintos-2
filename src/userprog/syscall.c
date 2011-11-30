a6513f338dba8a86af4cbeb6fbadd5b65c575c80
René Kijewski <kijewski@mi.fu-berlin.de>
2011-11-30 23:38:18 +0100

    syscall.c += user_strlen

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -26,7 +26,11 @@ static struct lock filesys_lock;
   lock_release (&filesys_lock); \
   _r;                           \
 })
-#define SYNC_VOID(WHAT) SYNC ( (WHAT, 0) )
+#define SYNC_VOID(WHAT) \
+({                      \
+  SYNC ( (WHAT, 0) );   \
+  (void) 0;             \
+})
 
 void
 syscall_init (void) 
@@ -61,6 +65,48 @@ is_user_memory (const void *addr, unsigned size)
   return true;
 }
 
+
+static void __attribute__ ((noreturn))
+kill_segv (void)
+{
+  struct thread *t = thread_current ();
+  printf ("Killed %d (%.*s), because of bad memory usage.\n",
+          t->tid, sizeof (t->name), t->name);
+  thread_exit ();
+}
+
+/**
+ * strlen (c). Ensures c and following (upto result+1) characters are in
+ * user memory.
+ * 
+ * \return -1, if (hits) invalid memory
+ */
+static signed
+user_strlen (const char *c)
+{
+  if (!c)
+    return -1;
+  
+  signed result = 0;
+  struct thread *t = thread_current ();
+  for (;;)
+    {
+      if ((void *) c >= PHYS_BASE)
+        return -1;
+      
+      const char *downfrom = (const char *) ((intptr_t) c & ~(PGSIZE-1));
+      const char *upto     = (const char *) ((intptr_t) c |  (PGSIZE-1));
+      
+      if (pagedir_get_page (t->pagedir, downfrom) == NULL)
+        return -1;
+      while (c <= upto)
+        if (*(c++))
+          ++result;
+        else
+          return result;
+    }
+}
+
 static void
 syscall_handler_SYS_HALT (_SYSCALL_HANDLER_ARGS)
 {
@@ -81,7 +127,14 @@ static void
 syscall_handler_SYS_EXEC (_SYSCALL_HANDLER_ARGS)
 {
   // pid_t exec (const char *file);
-  //TODO
+  
+  const char *file = *(const char **) arg1;
+  signed len = user_strlen (file);
+  if (len < 0)
+    kill_segv ();
+  if (len >= sizeof (((struct thread *) 0)->name));
+    thread_exit ();
+  if_->eax = process_execute (file);
 }
 
 static void
@@ -96,11 +149,12 @@ syscall_handler_SYS_CREATE (_SYSCALL_HANDLER_ARGS)
 {
   // bool create (const char *file, unsigned initial_size);
   
-  //TODO: ensure user memory (EFAULT)
   const char *filename = *(const char **) arg1;
-  if (strlen (filename) > READDIR_MAX_LEN)
+  signed len = user_strlen (filename);
+  if (len < 0)
+    kill_segv ();
+  if (len > READDIR_MAX_LEN)
     thread_exit ();
-    
   if_->eax = SYNC (filesys_create (filename, *(off_t *) arg2));
 }
 
@@ -109,11 +163,12 @@ syscall_handler_SYS_REMOVE (_SYSCALL_HANDLER_ARGS)
 {
   // bool remove (const char *file);
   
-  //TODO: ensure user memory (EFAULT)
   const char *filename = *(const char **) arg1;
-  if (strlen (filename) > READDIR_MAX_LEN)
+  signed len = user_strlen (filename);
+  if (len < 0)
+    kill_segv ();
+  if (len > READDIR_MAX_LEN)
     thread_exit ();
-    
   if_->eax = SYNC (filesys_remove (filename));
 }
 
@@ -122,9 +177,11 @@ syscall_handler_SYS_OPEN (_SYSCALL_HANDLER_ARGS)
 {
   // int open (const char *file);
   
-  //TODO: ensure user memory (EFAULT)
   const char *filename = *(const char **) arg1;
-  if (strlen (filename) > READDIR_MAX_LEN)
+  signed len = user_strlen (filename);
+  if (len < 0)
+    kill_segv ();
+  if (len > READDIR_MAX_LEN)
     {
       if_->eax = -ENAMETOOLONG;
       return;
@@ -174,6 +231,7 @@ static void
 syscall_handler_SYS_FILESIZE (_SYSCALL_HANDLER_ARGS)
 {
   // int filesize (int fd);
+  
   struct fd *fd_data = retrieve_fd (*(unsigned *) arg1);
   if_->eax = fd_data ? SYNC (file_length (fd_data->file)) : -1;
 }
@@ -188,11 +246,7 @@ syscall_handler_SYS_READ (_SYSCALL_HANDLER_ARGS)
   unsigned length = *(unsigned *) arg3;
   
   if (!is_user_memory (buffer, length))
-    {
-      printf ("Killed %d, because of bad memory usage.\n",
-              thread_current ()->tid);
-      thread_exit ();
-    }
+    kill_segv ();
   struct fd *fd_data = retrieve_fd (fd);
   if (!fd_data)
     {
@@ -212,11 +266,7 @@ syscall_handler_SYS_WRITE (_SYSCALL_HANDLER_ARGS)
   unsigned length = *(unsigned *) arg3;
   
   if (!is_user_memory (buffer, length))
-    {
-      printf ("Killed %d, because of bad memory usage.\n",
-              thread_current ()->tid);
-      thread_exit ();
-    }
+    kill_segv ();
   else if (fd == 0 || fd >= INT_MAX)
     {
       if_->eax = -EBADF;
@@ -244,11 +294,7 @@ syscall_handler_SYS_SEEK (_SYSCALL_HANDLER_ARGS)
   
   struct fd *fd_data = retrieve_fd (fd);
   if (!fd_data)
-    {
-      printf ("Killed %d, because of bad fd.\n",
-              thread_current ()->tid);
-      thread_exit ();
-    }
+    kill_segv ();
   SYNC_VOID (file_seek (fd_data->file, position));
 }
 
@@ -256,13 +302,10 @@ static void
 syscall_handler_SYS_TELL (_SYSCALL_HANDLER_ARGS)
 {
   // unsigned tell (int fd);
+  
   struct fd *fd_data = retrieve_fd (*(unsigned *) arg1);
   if (!fd_data)
-    {
-      printf ("Killed %d, because of bad fd.\n",
-              thread_current ()->tid);
-      thread_exit ();
-    }
+    kill_segv ();
   if_->eax = SYNC (file_tell (fd_data->file));
 }
 
@@ -270,16 +313,13 @@ static void
 syscall_handler_SYS_CLOSE (_SYSCALL_HANDLER_ARGS)
 {
   // void close (int fd);
+  
   struct fd search;
   memset (&search, 0, sizeof (search));
   search.fd = *(unsigned *) arg1;
   struct hash_elem *e = hash_delete (&thread_current ()->fds, &search.elem);
   if (!e)
-    {
-      printf ("Killed %d, because of bad fd.\n",
-              thread_current ()->tid);
-      thread_exit ();
-    }
+    kill_segv ();
   struct fd *fd_data = hash_entry (e, struct fd, elem);
   SYNC_VOID (file_close (fd_data->file));
   free (fd_data);
