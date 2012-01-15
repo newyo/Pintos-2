9b053a638508beb6fd077fc4c1c9205e4e1e5c40
René Kijewski <kijewski@mi.fu-berlin.de>
2012-01-15 21:33:37 +0100

    Less macros

--- src/vm/lru.h
+++ src/vm/lru.h
@@ -50,17 +50,35 @@ void lru_use (struct lru *l, struct lru_elem *e);
 void lru_dispose (struct lru *l, struct lru_elem *e, bool run_dispose_action);
 
 struct lru_elem *lru_peek_least (struct lru *l);
-#define lru_pop_least(L) \
-({ \
-  __typeof (L) _l = (L); \
-  struct lru_elem *_e = lru_peek_least (_l); \
-  if (_e) \
-    lru_dispose (_l, _e, false); \
-  _e; \
-})
 
-#define lru_is_empty(L) ((L)->item_count == 0)
-#define lru_capacity(L) ((L)->lru_size)
-#define lru_usage(L) ((L)->item_count)
+static inline struct lru_elem *
+lru_pop_least (struct lru *l)
+{
+  ASSERT (l != NULL);
+  struct lru_elem *e = lru_peek_least (l);
+  if (e)
+    lru_dispose (l, e, false);
+  return e;
+}
+
+static inline size_t
+lru_usage (struct lru *l)
+{
+  ASSERT (l != NULL);
+  return l->item_count;
+}
+
+static inline size_t
+lru_is_empty (struct lru *l)
+{
+  return lru_usage (l) == 0;
+}
+
+static inline size_t
+lru_capacity (struct lru *l)
+{
+  ASSERT (l != NULL);
+  return l->item_count;
+}
 
 #endif
